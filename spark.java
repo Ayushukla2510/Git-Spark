7a4ed2470c9c7bfaca8c2a7a24861256d5a8bb49
Ayush Shukla <ayushukla2510@gmail.com>
2025-10-20 02:57:05 +0530

    m2 service added

--- spark.java
+++ spark.java
@@ -2,9 +2,14 @@ class spark{
     public static void main(String[] args) {
         System.out.println("Hello");
         m1();
+        m2();
     }
 
     public static void m1(){
         System.out.println("m1");
+    }
+
+    public static void m2(){
+        System.out.println("m2");
     }
 }
